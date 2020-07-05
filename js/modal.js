d7c43fcd54a942bd10410ea16e3ed60cf226bb93
dianaki <diana.kuramshina21@gmail.com>
2020-07-05 03:09:42 +0300

    complete form

---
+++ js/modal.js
@@ -0,0 +1,75 @@
+const validateFields = (form, fieldsArray) => {
+
+  fieldsArray.forEach((field) => {
+    field.removeClass('input-error');
+    if (field.val().trim() === "") {
+      field.addClass('input-error');
+      $('.error').text('Заполните это поле').css('color', 'red');
+    }
+  });
+
+  const errorFields = form.find('.input-error');
+
+
+  return errorFields.length === 0;
+}
+
+$('.form').submit((e) => {
+  e.preventDefault();
+
+  const form = $(e.currentTarget);
+  const name = form.find("[name='name']");
+  const phone = form.find("[name='phone']");
+  const comment = form.find("[name='comment']");
+  const to = form.find("[name='to']");
+
+  const isValid = validateFields(form, [name, phone, comment, to]);
+
+  const modal = $('#modal');
+  const content = modal.find('.modal__content')
+  
+  if (isValid) {
+    $.ajax({
+      url: 'https://webdev-api.loftschool.com/sendmail',
+      method: 'post',
+      data: {
+        name: name.val(),
+        phone: phone.val(),
+        comment: comment.val(),
+        to: to.val(),
+      },
+      success: (data) => {
+        content.text(data.message);
+        $('.error').text('');
+        name.val('');
+        phone.val('');
+        comment.val('');
+      
+        $.fancybox.open({
+          src: ".modal",
+          type: "inline",
+          smallBtn: false,
+        });
+      },
+      error: (data) => {
+        $('.error').text('');
+        name.val('');
+        phone.val('');
+        comment.val('');
+        const message = data.responseJSON.message;
+        content.text(message);
+
+        $.fancybox.open({
+          src: ".modal",
+          type: "inline",
+          smallBtn: false,
+        });
+      },
+    }) 
+  }
+})
+$(".js-submit-btn").click((e)=> {
+  e.preventDefault();
+
+  $.fancybox.close();
+})
--- js/slideshow.js
+++ js/slideshow.js
@@ -3,10 +3,6 @@ $('.reviews__switcher-item').on('click', function(e){
   e.preventDefault();
   let clickIndex = $(this).index();
   let reviewItem = $('.reviews__display-inner').eq(clickIndex);
-  $(this).siblings().removeClass('interactive-avatar--active');
-  $(this).addClass('interactive-avatar--active')
-  
-
-  reviewItem.siblings().removeClass('reviews__display-inner--active');
-  reviewItem.addClass('reviews__display-inner--active')
+  $(this).addClass('interactive-avatar--active').siblings().removeClass('interactive-avatar--active');
+  reviewItem.addClass('reviews__display-inner--active').siblings().removeClass('reviews__display-inner--active');
 })
